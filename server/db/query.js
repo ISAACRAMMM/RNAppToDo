d4f55cf451a451257fadf409c731d37e5ed2070f
ISAACRAMMM <isaac-ram-lop@hotmail.com>
2024-09-04 13:20:35 -0600

    querys

--- server/db/query.js
+++ server/db/query.js
@@ -13,15 +13,15 @@ export async function getTodoByID(id){
 
 export async function getTodo(id){
     const result= await pool.query(
-
+        `SELECT * FOM todos WHERE id = $1`, [id]
     );
     const row = result.rows[0];
     return row;
 }
 
-export async function getSharedTodoByID(id)){
+export async function getSharedTodoByID(id){
     const result= await pool.query(
-
+        `SELECT * FOM shared_todos WHERE todo_id = $1`, [id]
     );
     const row = result.rows[0];
     return row;
@@ -29,7 +29,7 @@ export async function getSharedTodoByID(id)){
 
 export async function getUserByID(id){
     const result= await pool.query(
-
+        `SELECT * FROM users WHERE id = $1`, [id]
     );
     const row = result.rows[0];
     return row;
@@ -37,7 +37,7 @@ export async function getUserByID(id){
 
 export async function getUserByEmail(email){
     const result= await pool.query(
-
+        `SELECT * FROM users WHERE id = $1`, [email]
     );
     const row = result.rows[0];
     return row;
@@ -45,7 +45,7 @@ export async function getUserByEmail(email){
 
 export async function createTodo(user_id, title){
     const result= await pool.query(
-
+        `INSERT INTO todos (user_id, title) VALUES ($1, $2)`, [user_id,title]
     );
     const row = result.rows[0];
     return row;
@@ -53,15 +53,16 @@ export async function createTodo(user_id, title){
 
 export async function deleteTodo(id){
     const result= await pool.query(
-
+        `DELETE FROM todos WHERE id = $1`,[id]
     );
     const row = result.rows[0];
     return row;
 }
 
 export async function toggleCompleted(id, value){
+    const newValue = value === true ? "TRUE" : "FALSE";
     const result= await pool.query(
-
+        `UPDATE todos SET completed =$1 WHERE id = $2`,[newValue,id]
     );
     const row = result.rows[0];
     return row;
@@ -69,7 +70,8 @@ export async function toggleCompleted(id, value){
 
 export async function shareTodo(todo_id, user_id, shared_with_id){
     const result= await pool.query(
-
+        `INSERT INTO shared_todos (todo_id,user_id,shared_with_id) 
+        VALUES ($1,$2,$3)`,[todo_id,user_id,shared_with_id]
     );
     const row = result.rows[0];
     return row;
