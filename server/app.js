cfa43db0872a3a21585a3dc4c8134a751be549d0
ISAACRAMMM <isaac-ram-lop@hotmail.com>
2024-09-08 19:28:59 -0600

    peticiones get

--- server/app.js
+++ server/app.js
@@ -15,10 +15,35 @@ const app = express();
 
 app.use(express.json());
 
+
+
 app.get("/todos/:id", async (req, res) => {
     const todos = await getTodoByID(req.params.id);
     res.status(200).send(todos);
 });
+
+app.get("/todos/shared_todos/:id", async (req, res) => {
+    const todo = await getSharedTodoByID(req.params.id);
+    const author = await getUserByID(todo.user_id);
+    const shared_with = await getUserByID(todo.shared_with);
+
+    res.status(200).send({ author, shared_with });
+});
+
+app.get("/users/:id" ,async (req, res) => {
+    const user = await getUserByID(req.params.id);
+    
+    res.status(200).send(user);
+})
+
+app.put("/todos/:id", async (req, res) => {
+    const { value } = req.body;
+    const todo = await toggleCompleted(rec.params.id, value);
+    res.status(200).send(todo);
+})
+
+app.delete("/todos/:id", async  )
+
 
 app.listen(3400, () => {
     console.log('server listening on port 3400') 
